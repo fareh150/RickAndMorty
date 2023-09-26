0f7c172ca5ae9ea5354dd0f32f3203179e4e5379
fareh <fareh.rv@gmail.com>
2023-09-27 00:20:16 +0200

    continue

--- src/componenets/Main/Main.jsx
+++ src/componenets/Main/Main.jsx
@@ -1,9 +1,37 @@
 import "./Main.css"
+import { useState, useEffect } from "react"
+import { v4 as uuidv4 } from 'uuid';
+
 
 const Main = () => {
+
+    const [characters, setCharacters] = useState([]);
+
+    const getCharacters = async () => {
+        const data = await fetch(`https://rickandmortyapi.com/api/character?page=1`);
+        const dataJSON = await data.json();
+        setCharacters(dataJSON.results);
+    }
+
+    useEffect(() => {
+        getCharacters()
+    }, [])
+
     return (
         <main>
             <h1 className="title">Rick and Morty</h1>
+            <section className="galeria">
+                {characters.map((character) => (
+                    <ul key={character.id} className="card">
+                        <li className="imagen"><img src={character.image} alt={character.name} /></li>
+                        <li className="name">{character.name}</li>
+                        <li className="status">{character.status}</li>
+                        <li className="species">{character.species}</li>
+                        
+                    </ul>
+                    ))}
+            </section>
+            
         </main>
     )
 }
